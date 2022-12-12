67de02834d6409fa2499a111e115934b30845dd7
Maxwell G <gotmax@e.email>
2022-12-11 18:50:25 -0600

    Rearchitect lint.sh and add reuse lint

--- lint.sh
+++ lint.sh
@@ -1,23 +1,29 @@
-#!/usr/bin/bash -x
-
+#!/usr/bin/bash
 # SPDX-FileCopyrightText: 2022 Maxwell G <gotmax@e.email>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 set -eu
-if [ "${check-}" == "true" ]; then
+
+if [ "${1-}" == "--check" ]; then
   c="--check"
 else
   c=""
 fi
 r=0
 
-fail() {
-  r=1
+run() {
+    local r
+    r=0
+    echo "**** Running: $@ ****"
+    "$@" || { r=$?; echo "**** Failed: $r ****" ; }
+    echo
+    return $r
 }
 
 
-isort fedrq ${c} || fail
-black fedrq ${c} || fail
-flake8 --max-line-length 89 fedrq || fail
-mypy fedrq || fail
+run isort fedrq ${c}
+run black fedrq ${c}
+run flake8 --max-line-length 89 fedrq
+run mypy fedrq
+run reuse lint
 exit $r
