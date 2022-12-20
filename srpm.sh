a4482d310c7fecb2be3f0dd17c44ad856ba08080
Maxwell G <gotmax@e.email>
2022-12-19 22:35:26 -0600

    srpm.sh: Handle releases

--- srpm.sh
+++ srpm.sh
@@ -7,18 +7,22 @@ set -euo pipefail
 outdir="${1:-results_fedrq}"
 projectdir="$(pwd)"
 specfile="fedrq.spec"
-lastref="7eb724c"
+lastref="v0.0.1"
 
 mkdir -p "${outdir}"
 find -maxdepth 1 \( -name 'fedrq-*.tar.gz' -o -name '*.src.rpm' \) -delete -print
 find "${outdir}" -maxdepth 1 -type f -name '*.src.rpm' -delete -print
 
 origversion="$(rpmspec -q --qf '%{version}\n' "${specfile}" | sed 's|~1$||')"
-newversion="$(printf '%s~%s.%s\n' \
+if [ "$(git rev-list -n1 ${lastref})" = "$(git rev-parse HEAD)" ]; then
+    newversion="${origversion}"
+else
+    newversion="$(printf '%s^%s.%s\n' \
                 "${origversion}" \
                 "$(( $(git log --oneline "${lastref}..HEAD" | wc -l) + 1 ))" \
                 "$(git show -s --pretty=%cs.%h | sed 's|-||g')"
             )"
+fi
 archivename="fedrq-${newversion}"
 
 cp "${specfile}" "${specfile}.bak"
