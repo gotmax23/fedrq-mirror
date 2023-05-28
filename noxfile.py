a49599c4c4f1347ac20aedf11ee27bcbfe30c54a
Maxwell G <maxwell@gtmx.me>
2023-05-28 22:00:26 +0000

    nox: refactor bump and publish workflows

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,9 @@ from __future__ import annotations
 import itertools
 import os
 import subprocess
+from collections.abc import Collection, Iterable
 from pathlib import Path
+from shutil import copy2, rmtree
 
 import nox
 import nox.command
@@ -19,6 +21,7 @@ ALLOW_EDITABLE = os.environ.get("ALLOW_EDITABLE", str(not IN_CI)).lower() in (
     "true",
 )
 LINT_SESSIONS = ("formatters", "codeql", "typing", "reuse")
+PROJECT = "fedrq"
 
 nox.options.sessions = (*LINT_SESSIONS, "test", "libdnf5_test")
 
@@ -38,6 +41,10 @@ def run_silent(*args, return_stdout: bool = False, **kwargs):
     kwargs.setdefault("check", True)
     proc = subprocess.run(args, **kwargs)
     return proc.stdout if return_stdout else proc
+
+
+def git(session: nox.Session, *args, **kwargs):
+    return session.run("git", *args, **kwargs, external=True)
 
 
 def _to_install_system(session, *packages: str):
@@ -100,7 +107,7 @@ def testa(session):
 @nox.session(venv_params=["--system-site-packages"])
 def libdnf5_test(session: nox.Session):
     install_system(session, "python3-libdnf5")
-    test.func(session, "libdnf5")
+    test(session, "libdnf5")
 
 
 @nox.session(venv_backend="none")
@@ -217,14 +224,14 @@ def srpm(session: nox.Session, posargs=None):
 def mockbuild(session: nox.Session):
     install_system(session, "mock", "mock-core-configs")
     tmp = Path(session.create_tmp())
-    srpm.func(session, ("-o", tmp, "--keep"))
+    srpm(session, ("-o", tmp, "--keep"))
     spec_path = tmp / "fedrq.spec"
     margs = [
         "mock",
         "--spec",
-        spec_path,
+        str(spec_path),
         "--source",
-        tmp,
+        str(tmp),
         *session.posargs,
     ]
     if not session.interactive:
@@ -241,15 +248,131 @@ def mockbuild(session: nox.Session):
     session.run(*margs, external=True)
 
 
-@nox.session(venv_backend="none")
+def ensure_clean(session: nox.Session):
+    if git(session, "status", "--porcelain", "--untracked-files", silent=True):
+        msg = "There are untracked and/or modified files."
+        session.error(msg)
+
+
+def _check_git_tag(session: nox.Session, version: str):
+    tag = "v" + version
+    tags = git(session, "tag", "--list", silent=True).splitlines()
+    if tag in tags:
+        session.error(f"{tag} is already tagged")
+
+
+@nox.session
 def bump(session: nox.Session):
-    session.run("bash", "-x", "contrib/bump.sh", *session.posargs)
+    version = session.posargs[0]
+    _check_git_tag(session, version)
+    ensure_clean(session)
+
+    install(session, "flit>=3.9", "tomcli[tomlkit]", "twine")
+
+    # Download generate_changelog
+    tmp = Path(session.create_tmp())
+    with session.chdir(tmp):
+        session.run_always(
+            "wget",
+            "https://git.sr.ht/~gotmax23/fedora-scripts/blob/main/generate_changelog.py",
+            external=True,
+        )
+
+    # Update version
+    session.run("tomcli-set", "pyproject.toml", "str", "project.version", version)
+
+    install(session, ".")
+
+    # Bump specfile
+    session.run(
+        "fclogr",
+        "bump",
+        "--new",
+        version,
+        "--comment",
+        f"Release {version}.",
+        f"{PROJECT}.spec",
+    )
+
+    # Bump changelog, commit, and tag
+    git(session, "add", f"{PROJECT}.spec", "pyproject.toml")
+    session.run(
+        "python", str(tmp / "generate_changelog.py"), f"--version={version}", "--tag"
+    )
+
+    if Path("dist").is_dir():
+        rmtree("dist")
+    session.run("flit", "build", "--use-vcs")
+    artifacts = _get_artifacts(("*.whl", "*.tar.gz"), expected_count=2)
+    session.run("twine", "check", "--strict", *artifacts)
+    _sign_artifacts(session)
+
+
+def _sign_artifacts(session: nox.Session) -> None:
+    uid = git(session, "config", "user.email", silent=True).strip()
+    artifacts = _get_artifacts(["tar.gz", "whl"], expected_count=2)
+    for path in artifacts:
+        if Path(path + ".asc").exists():
+            session.warn(f"{path}.asc already exists. Not signing it.")
+            continue
+        session.run(
+            "gpg", "--local-user", uid, "--armor", "--detach-sign", path, external=True
+        )
+
+
+def _get_artifacts(
+    exts: Collection[str] = ("whl", "tar.gz", "tar.gz.asc", "whl.asc"),
+    required=True,
+    extra_allowed=True,
+    expected_count: int | None = None,
+) -> Iterable[str]:
+    count = 0
+    all_files = set(Path("dist").iterdir())
+    for ext in exts:
+        found = tuple(Path("dist").glob(f"*.{ext}"))
+        if not found and required:
+            raise ValueError(f"No matches for {ext}")
+        count += len(found)
+        for file in found:
+            all_files.remove(file)
+        yield from map(str, found)
+    if all_files and not extra_allowed:
+        raise ValueError(f"Extra files found in dist: {all_files}")
+    if expected_count is not None and count != expected_count:
+        raise ValueError(f"Found {count} artifacts. Expected {expected_count}.")
 
 
 @nox.session
 def publish(session: nox.Session):
-    install(session, "copr", "dbus-python", "flit", "keyring", "twine")
-    session.run("bash", "-x", "contrib/publish.sh", *session.posargs)
+    # Setup
+    ensure_clean(session)
+    install(session, "twine", "tomcli[tomlkit]")
+    session.run(
+        "twine",
+        "upload",
+        "--non-interactive",
+        "--username",
+        "__token__",
+        *_get_artifacts(("*.whl", "*.tar.gz"), extra_allowed=False, expected_count=2),
+    )
+
+    # Copr build
+    copr_release(session)
+
+    # Push to git
+    if session.interactive and input("Push to Sourcehut (Y/n)").lower() != "n":
+        git(session, "push", "--follow-tags")
+        srht_artifacts(session)
+
+    # Post-release bump
+    version = session.run(  # type: ignore[union-attr]
+        "tomcli-get", "pyproject.toml", "project.version", silent=True
+    ).strip()
+    session.run(
+        "tomcli-set", "pyproject.toml", "str", "project.version", f"{version}.post0"
+    )
+    git(session, "add", "pyproject.toml")
+    git(session, "commit", "-S", "-m", "Post release version bump")
 
 
 @nox.session
@@ -280,7 +403,23 @@ def docgen(session: nox.Session):
 
 
 @nox.session
+def copr_release(session: nox.Session):
+    install(session, "copr-cli", "requests-gssapi", "specfile")
+    tmp = Path(session.create_tmp())
+    dest = tmp / "tomcli.spec"
+    copy2("tomcli.spec", dest)
+    session.run("python", "contrib/fedoraify.py", str(dest))
+    session.run("copr-cli", "build", "--nowait", "gotmax23/tomcli", str(dest))
+
+
+@nox.session(python="none")
+def srht_artifacts(session: nox.Session):
+    artifacts = map(str, Path("dist").glob("*"))
+    session.run("hut", "git", "artifact", "upload", *artifacts, external=True)
+
+
+@nox.session
 def mkdocs(session: nox.Session):
     install(session, "-e", ".[doc]")
-    docgen.func(session)
+    docgen(session)
     session.run("mkdocs", *session.posargs)
