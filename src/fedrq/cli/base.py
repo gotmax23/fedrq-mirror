gotmax23/fedrq-mirror

commit eea7ee13aa81e69ceab7cb8a571308aca4facb10

cli: add local pseudo branch
This allows querying the enabled system repos in the same way that plain
`dnf repoquery` does.

src/fedrq/cli/base.py
@@ -325,15 +325,13 @@ class Command(abc.ABC):
         return None
 
     @v_fatal_error
-    def v_release(self) -> str | None:
+    def v_rq(self) -> str | None:
         try:
-            self.release = self.config.get_release(self.args.branch, self.args.repos)
+            if self.args.branch != "local":
+                release = self.config.get_release(self.args.branch, self.args.repos)
         except ConfigError as err:
             return str(err)
-        return None
 
-    @v_fatal_error
-    def v_rq(self) -> str | None:
         conf: dict[str, Any] = {}
         bvars: dict[str, Any] = {}
 
@@ -350,10 +348,13 @@ class Command(abc.ABC):
             bvars["arch"] = self.args.forcearch
         bm = self.backend.BaseMaker()
         try:
-            self.release.make_base(self.config, conf, bvars, bm, False)
+            if self.args.branch == "local":
+                bm.read_system_repos(False)
+            else:
+                release.make_base(self.config, conf, bvars, bm, False)
             for func, repo in self.args.enable_disable:
                 if func == "enable":
-                    self.release.get_repog(repo).load(bm, self.config, self.release)
+                    release.get_repog(repo).load(bm, self.config, release)
                 elif func == "disable":
                     bm.disable_repo(repo, True)
                 else:
@@ -377,7 +378,6 @@ class Command(abc.ABC):
         self.v_arch()
         # Fatal
         self.v_backend()
-        self.v_release()
         self.v_rq()
         self._v_handle_errors()
 

doc/fedrq.1.scd
@@ -260,6 +260,11 @@ The following generic repo class macros are available for all releases:
 :  amazon2023
 :  *@base*
 : 
+|  Default local system repositories
+:  local
+:  N/A
+:  This uses the default repositories with *enabled=1* in /etc/yum.repos.d and
+   and the system's releasever.
 
 ## SPECIAL REPOS
 

tests/unit/test_pkgs.py
@@ -4,6 +4,7 @@
 
 import pytest
 
+import fedrq.backends
 import fedrq.cli
 
 
@@ -27,3 +28,26 @@ def test_repo_by_real_name(patch_config_dirs, capsys, args):
     out, err = capsys.readouterr()
     assert set(out.splitlines()) == {"testrepo1"}
     assert not err
+
+
+@pytest.mark.no_rpm_mock
+def test_local_release(monkeypatch: pytest.MonkeyPatch):
+    backend = fedrq.backends.get_default_backend()
+    releasever = backend.get_releasever()
+    base_maker = backend.BaseMaker()
+    base_maker.read_system_repos(False)
+    repolist = base_maker.repolist()
+    del base_maker
+
+    parser = fedrq.cli.Pkgs.make_parser()
+    args = parser.parse_args(["-b", "local", "ansible"])
+    # Calling fill_sack() is expensive and not necessary here
+    monkeypatch.setattr(backend.BaseMaker, "fill_sack", lambda self: self.base)
+    obj = fedrq.cli.Pkgs(args)
+    base_maker = backend.BaseMaker(obj.rq.base)
+    assert sorted(base_maker.repolist()) == sorted(repolist)
+    # TODO: Add a proper API for this
+    if backend.BACKEND == "dnf":
+        assert releasever == base_maker.base.conf.substitutions["releasever"]
+    else:
+        assert releasever == base_maker.base.get_vars().get_value("releasever")

commit 9df2311cb486cee044a58020ff696fdd3d500fd8

make local a proper release in the config file
Treating local as a psuedo release did not work properly. Specifically,
the --enablerepo and related options didn't work. This utilizes the new
`version` and `append_system_repos` options to make local a proper
release.

Fixes: eea7ee13aa81e69ceab7cb8a571308aca4facb10

src/fedrq/cli/base.py
@@ -325,13 +325,15 @@ class Command(abc.ABC):
         return None
 
     @v_fatal_error
-    def v_rq(self) -> str | None:
+    def v_release(self) -> str | None:
         try:
-            if self.args.branch != "local":
-                release = self.config.get_release(self.args.branch, self.args.repos)
+            self.release = self.config.get_release(self.args.branch, self.args.repos)
         except ConfigError as err:
             return str(err)
+        return None
 
+    @v_fatal_error
+    def v_rq(self) -> str | None:
         conf: dict[str, Any] = {}
         bvars: dict[str, Any] = {}
 
@@ -348,13 +350,10 @@ class Command(abc.ABC):
             bvars["arch"] = self.args.forcearch
         bm = self.backend.BaseMaker()
         try:
-            if self.args.branch == "local":
-                bm.read_system_repos(False)
-            else:
-                release.make_base(self.config, conf, bvars, bm, False)
+            self.release.make_base(self.config, conf, bvars, bm, False)
             for func, repo in self.args.enable_disable:
                 if func == "enable":
-                    release.get_repog(repo).load(bm, self.config, release)
+                    self.release.get_repog(repo).load(bm, self.config, self.release)
                 elif func == "disable":
                     bm.disable_repo(repo, True)
                 else:
@@ -378,6 +377,7 @@ class Command(abc.ABC):
         self.v_arch()
         # Fatal
         self.v_backend()
+        self.v_release()
         self.v_rq()
         self._v_handle_errors()
 

src/fedrq/data/releases.toml
@@ -236,3 +236,9 @@ defs.base = [
     "eln-crb",
     "eln-crb-source",
 ]
+
+[releases.local]
+matcher = "local"
+version = "$releasever"
+append_system_repos = true
+defs.base = []