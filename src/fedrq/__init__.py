9f3574138e097cbfa07bdfcb3aa2ebf3fc8cac98
Maxwell G <maxwell@gtmx.me>
2023-08-30 23:12:00 -0500

    Post release version bump

--- src/fedrq/__init__.py
+++ src/fedrq/__init__.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 
 import logging
 
-__version__ = "0.11.0"
+__version__ = "0.11.0.post0"
 
 fmt = "{levelname}:{name}:{lineno}: {message}"
 logging.basicConfig(format=fmt, style="{")
