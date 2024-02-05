4812fa8aadb873bc8ad3bce4b16fa5aa4f18d00e
Maxwell G <maxwell@gtmx.me>
2024-02-05 02:26:41 +0000

    ci: refresh locked dependencies

--- noxfile.py
+++ noxfile.py
@@ -270,7 +270,9 @@ def pydanticv1_test(session: nox.Session):
     test(session, "dnf", ["tests/unit"])
 
 
-@nox.session(name="pip-compile", python=["3.9"], venv_params=["--download"])
+@nox.session(
+    name="pip-compile", python=["3.9"], venv_params=["--download"], reuse_venv=False
+)
 def pip_compile(session: nox.Session):
     session.install("pip-tools")
     Path("requirements").mkdir(exist_ok=True)
