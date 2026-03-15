91a1b07f27348762c08b28f7afaea2ec46a4b3c1
Maxwell G <maxwell@gtmx.me>
2026-03-14 20:28:19 -0500

    contrib: add missing license header

--- contrib/edbackends.sh
+++ contrib/edbackends.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
-
+# Copyright (C) 2025 Maxwell G <maxwell@gtmx.me>
+# SPDX-License-Identifier: GPL-2.0-or-later
 set -euo pipefail
 
 exec $EDITOR src/fedrq/backends/base/__init__.py src/fedrq/backends/*/backend/__init__.py
